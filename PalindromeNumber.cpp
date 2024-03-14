2b2a0e312ca0754c2e1eeb2ec57ba22d275d5122
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-14 20:01:20 +0530

    Create PalindromeNumber.cpp

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long s=0;
+        int n=x;
+        while(n!=0)
+        {
+            s=s*10+(n%10);
+            n/=10;
+        }
+        if(
+  
+       
+
+
+
+        
+
+};
