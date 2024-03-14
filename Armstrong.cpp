1cd332459e603817a6d24587e6b5d1fd00da0c8c
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-14 20:04:58 +0530

    Create Armstrong.cpp

---
+++ Armstrong.cpp
@@ -0,0 +1,17 @@
+bool checkArmstrong(int n){
+	//Write your code here
+	int x=n;
+	int c=0;
+	while(x!=0)
+	{
+		c++;
+		x/=10;
+	}
+	 x=n;int s=0;
+	while(x!=0)
+	{
+		s=s+pow((x%10),c);
+		x/=10;
+	}
+	return (s==n);
+}
