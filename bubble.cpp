6221bf47793e0a8d8ed0e49845d96da5624203be
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-15 22:38:37 +0530

    bubble sort using iteration and recursion

---
+++ bubble.cpp
@@ -0,0 +1,31 @@
+//using recursion
+void bubbleSort(vector<int>& arr, int n) 
+{
+    //write your code here
+    if(n==1)return;
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    bubbleSort(arr,n-1);
+    
+}
+
+//using iterative approach
+void bubble_sort(int arr[], int n) {
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--) {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0) {
+            break;
+        }
+    }
