b41e9496e730659b64a1b3a0b402995799c697f6
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-15 22:41:22 +0530

    insertion sort two approaches

---
+++ Insertion.cpp
@@ -0,0 +1,30 @@
+//using recursion
+void insertionSort(int arr[],int i, int n)
+{
+    //write your code here
+      // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertionSort(arr, i + 1, n);
+}
+
+//iterative approach
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
