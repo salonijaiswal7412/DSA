756ed8b8343f111d19aabd4c91a74e74aedb149c
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-15 22:39:43 +0530

    selection Sort.

---
+++ selectionSort.cpp
@@ -0,0 +1,14 @@
+void selectionSort(vector<int>&arr) {
+    // Write your code here.
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<arr.size();j++)
+        {
+            if(arr[j]<arr[min])
+            min=j;
+            
+        }
+        swap(arr[min],arr[i]);
+    }
+}
