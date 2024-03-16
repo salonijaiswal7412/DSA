4dfd347c1c757f8d69f9f05b13b5a9129470b550
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-03-16 22:06:01 +0530

    maximum consecutive ones

---
+++ maxConsecutiveOnes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int c=0;int m=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1){
+                c++;
+                m=max(m,c);
+            }
+            else
+            c=0;
+        }
+        return m;
+        
+    }
+};
