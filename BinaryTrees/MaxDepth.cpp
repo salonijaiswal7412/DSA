9c48800580f77ea42b53c53dfd25c634f35c7b93
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2024-04-24 18:57:39 +0530

    Update MaxDepth.cpp

--- BinaryTrees/MaxDepth.cpp
+++ BinaryTrees/MaxDepth.cpp
@@ -1,3 +1,4 @@
+//using recursion
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -8,3 +9,24 @@ public:
         
     }
 };
+
+//using level order traversal
+int heightOfBinaryTree(TreeNode<int> *root)
+{
+	// Write your code here.
+    if(root==nullptr)return 0;
+    int c=0;
+    queue<TreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        int s=q.size();
+        for(int i=0;i<s;i++){
+            TreeNode<int>* n=q.front();
+            q.pop();
+            if(n->left)q.push(n->left);
+            if(n->right)q.push(n->right);
+        }
+        c++;
+    }
+    return c;
+}
