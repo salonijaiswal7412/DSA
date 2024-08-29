1a34aeb63e2683d08aa9ff41b08db1bb5097105e
saloni <salonijaiswal.7412@gmail.com>
2024-08-29 20:19:49 +0530

    Create ContMostWater.cpp

---
+++ TwoPointer,SlidingWindow/11-ContMostWater.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea=0;
+        int l=0,r=height.size()-1;
+        while(l<r){
+            maxArea=max(maxArea,((r-l)*min(height[l],height[r])));
+            if(height[l]<height[r])
+            l++;
+            else
+            r--;
+        }
+        return maxArea;
+        
+    }
+};
