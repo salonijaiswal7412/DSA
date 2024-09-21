ddac9f92e12014c27489071ca0212b38f01f9e44
saloni <salonijaiswal.7412@gmail.com>
2024-09-21 17:53:08 +0530

    create ArrayProdExceptSelf.cpp

---
+++ arrays/ArrayProdExceptSelf.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(),1);
+        int p=1;
+        for(int i=0;i<nums.size();i++){
+            ans[i]*=p;
+            p*=nums[i];
+        }
+        p=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            ans[i]*=p;
+            p*=nums[i];
+        }
+        return ans;
+        
+        
+    }
+};
