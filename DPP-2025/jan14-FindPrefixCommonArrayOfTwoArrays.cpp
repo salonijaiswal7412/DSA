5eb5b2447fa4507514f407423a976ea816570253
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-01-14 16:35:18 +0530

    Update jan14-FindPrefixCommonArrayOfTwoArrays.cpp

--- DPP-2025/jan14-FindPrefixCommonArrayOfTwoArrays.cpp
+++ DPP-2025/jan14-FindPrefixCommonArrayOfTwoArrays.cpp
@@ -15,3 +15,21 @@ public:
         
     }
 };
+
+//as per solution
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        vector<int> ans;
+        int n=A.size();
+        vector<int>freq(n+1,0);
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(++freq[A[i]]==2)c++;
+            if(++freq[B[i]]==2) c++;
+            ans.push_back(c);
+        }
+        return ans;
+        
+    }
+};
