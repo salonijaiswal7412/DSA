eb45d702356824f1463f7e413812e5d9583f7b3e
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-02-01 16:31:48 +0530

    Create feb1-SpecialArray1.cpp

---
+++ DPP-2025/feb1-SpecialArray1.cpp
@@ -0,0 +1,27 @@
+//my approach
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        
+        for(int i=0;i<nums.size()-1;i++){
+            if((nums[i]%2==0 && nums[i+1]%2==0)||(nums[i]%2==1 && nums[i+1]%2==1))
+            return false;
+        }
+        return true;
+        
+    }
+};
+
+//bit manipulation approach
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        
+        for(int i=0;i<nums.size()-1;i++){
+            if((nums[i] & 1)==(nums[i+1] & 1)
+            return false;
+        }
+        return true;
+        
+    }
+};
