d3960b3fb0f1ea9243bb7cf26a01dac2f14819e3
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-03-03 10:31:22 +0530

    Create 21.Largest Color Val In A Directed Graph.cpp

---
+++ GraphCourse-Udemy/21.Largest Color Val In A Directed Graph.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestPathValue(string colors, vector<vector<int>> edges) {
+    int n=colors.size();
+    vector<vector<int>> v(n);
+    vector<vector<int>> cnt(n,vector<int>(26));
+    queue<int> q;
+    vector<int> ind(n);
+    for(auto& i:edges){
+        v[i[0]].push_back(i[1]);
+        ind[i[1]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(ind[i]==0){
+            q.push(i);
+            cnt[i][colors[i]-'a']=1;
+        }
+    }
+    int ans=0;
+    vector<int> topo;
+    while(!q.empty()){
+        int f=q.front();
+        topo.push_back(f);
+        q.pop();
+        for(int i=0;i<26;i++){
+            ans=max(ans,cnt[f][i]);
+        }
+        for(auto i:v[f]){
+            for(int j=0;j<26;j++){
+                cnt[i][j]=max(cnt[i][j],cnt[f][j]+(colors[i]-'a'==j));
+            }
+            if(--ind[i]==0){
+                q.push(i);
+            }
+        }
+    }
+    return topo.size()==n?ans:-1;
+    
+    
+}
