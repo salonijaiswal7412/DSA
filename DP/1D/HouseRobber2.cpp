d0bf84b407ddb59e668a4c626bf89939d89689a5
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-03-16 16:45:26 +0530

    Update HouseRobber2.cpp

--- DP/1D/HouseRobber2.cpp
+++ DP/1D/HouseRobber2.cpp
@@ -1,4 +1,27 @@
-
+//memoization
+class Solution {
+public:
+    int solve(int i,vector<int>& a,vector<int>& dp){
+        if(i==0) return a[0];
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int p=a[i]+solve(i-2,a,dp);
+        int np=solve(i-1,a,dp);
+        return dp[i]=max(p,np);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int> a(nums.begin()+1,nums.end());
+        vector<int> b(nums.begin(),nums.end()-1);
+        vector<int> dp1(n,-1),dp2(n,-1);
+        return max(solve(a.size()-1,a,dp1),solve(b.size()-1,b,dp2));
+        
+    }
+};
+//space optimised
 class Solution {
 public:
     int f(vector<int>& nums){
