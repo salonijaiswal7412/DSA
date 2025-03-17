4d1b2ec110de4cab89115b9a98dd2823804628c4
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-03-17 09:21:47 +0530

    Create Mar17-DivideArrIntoEqualParts.cpp

---
+++ DPP-2025/Mar17-DivideArrIntoEqualParts.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto i:nums){
+            m[i]++;
+        }
+        for(auto& i:m){
+            if(i.second%2) return false;
+        }
+        return true;
+        
+    }
+};
