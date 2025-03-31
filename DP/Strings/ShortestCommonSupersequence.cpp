7c7105e09141874f45cab368de0cc74b74d86d21
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-03-31 18:09:16 +0530

    Create ShortestCommonSupersequence.cpp

---
+++ DP/Strings/ShortestCommonSupersequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m=str1.size(),n=str2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+
+            }
+        }
+        string ans="";
+        int i=m,j=n;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1[(i--)-1];
+            }
+            else{
+                ans+=str2[(j--)-1];
+            }
+           
+        }
+        while(i>0) ans+=str1[(i--)-1];
+        while(j>0) ans+=str2[(j--)-1];
+        reverse(ans.begin(),ans.end());
+        return ans;
+            
+        
+    }
+};
