d5bcd574cdbb5d797337614bd3583a6de57b1aad
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-04-01 10:58:43 +0530

    Update EditDistance.cpp

--- DP/Strings/EditDistance.cpp
+++ DP/Strings/EditDistance.cpp
@@ -1,3 +1,4 @@
+//memoization
 class Solution {
 public:
     int f(int i,int j,string & s1,string& s2,vector<vector<int>>& dp){
@@ -17,3 +18,27 @@ public:
         
     }
 };
+
+//tabulation
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m=word1.size(),n=word2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=0;j<=n;j++) dp[0][j]=j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+(min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1])));
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+};
+    
