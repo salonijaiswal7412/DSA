93ae78d39603cbd78fe1c06222b209774f96c60a
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-04-10 12:41:46 +0530

    Create Permutation2.cpp

---
+++ Recursion and Backtracking/Permutation2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void f(vector<int>& nums,vector<int>& freq,vector<int>& temp,vector<vector<int>>& ans){
+        if(temp.size()==nums.size()){
+            ans.push_back(temp);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !freq[i-1]) continue;
+            freq[i]=1;
+            temp.push_back(nums[i]);
+            f(nums,freq,temp,ans);
+            temp.pop_back();
+            freq[i]=0;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int> freq(n,0);
+        vector<int> temp;
+        vector<vector<int>> ans;
+        f(nums,freq,temp,ans);
+        return ans;
+    }
+};
