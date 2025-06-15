d7fcc107e75ff102d169461b31612d14fed3edbf
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-06-15 23:58:10 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,7 +7,7 @@ void merge(int arr[], int l, int m, int h) {
     // Copy data to temp arrays L[] and R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
+    for (int j = 0; j < n2; j++) 
         R[j] = arr[m + 1 + j];
 
     // Merge the temp arrays back into arr[l..h]
