bf5cf669352ba1b871984f8d357a4b6a3197c5ca
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-07-13 18:05:18 +0530

    Create Implementation1.cpp

---
+++ Trie/Implementation1.cpp
@@ -0,0 +1,66 @@
+class Trie {
+public:
+    class Node{
+        public:
+        vector<Node*> children;
+        bool end;
+        Node(){
+            children.resize(26,0);
+            end=0;
+        }
+    };
+    Node* root;
+    Trie() {
+        root=new Node();
+
+        
+    }
+    
+    void insert(string word) {
+        Node* temp=root;
+        for(int i=0;i<word.size();i++){
+            int idx=word[i]-'a';
+            if(!temp->children[idx]){
+                temp->children[idx]=new Node();
+            }
+            temp=temp->children[idx];
+        }
+        temp->end=true;
+        
+    }
+    
+    bool search(string word) {
+        Node* temp=root;
+        for(int i=0;i<word.size();i++){
+            int idx=word[i]-'a';
+            if(!temp->children[idx]){
+                return false;
+            }
+            temp=temp->children[idx];
+        }
+        return temp->end;
+
+        
+    }
+    
+    bool startsWith(string word) {
+         Node* temp=root;
+        for(int i=0;i<word.size();i++){
+            int idx=word[i]-'a';
+            if(!temp->children[idx]){
+                return false;
+            }
+            temp=temp->children[idx];
+        }
+        return true;
+        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
