3fecdb42f62a4731e576ac4398d2c586625a781c
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-07-24 23:28:39 +0530

    Update missingNumber.cpp

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& nums) { 
         int ans=0;
         for(int i=0;i<nums.size();i++){ 
             ans^=(i+1);
