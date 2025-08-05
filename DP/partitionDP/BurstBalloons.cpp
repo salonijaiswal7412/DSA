a7483bf811c96bcdce1d1c656b1d027fb46ae0c5
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-08-05 17:29:30 +0530

    Create BurstBalloons.cpp

---
+++ DP/partitionDP/BurstBalloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=-1e9;
+        for(int k=i;k<=j;k++){
+            int t=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums,dp)+f(k+1,j,nums,dp);
+            ans=max(ans,t);
+        }
+        return dp[i][j]=ans;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.push_back(1);
+        nums.insert(nums.begin(),1);
+        int n=nums.size();
+
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return f(1,n-2,nums,dp);
+
+        
+    }
+};
