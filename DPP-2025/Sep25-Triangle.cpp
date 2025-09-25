e494d6e2087300f980f8ecaf30f28782d5de1669
Saloni Jaiswal <119559076+salonijaiswal7412@users.noreply.github.com>
2025-09-25 18:50:22 +0530

    Create Sep25-Triangle.cpp

---
+++ DPP-2025/Sep25-Triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle[n-1][i];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int dg=dp[i+1][j+1];
+                int d=dp[i+1][j];
+
+                dp[i][j]=triangle[i][j]+min(d,dg);
+            }
+        }
+        return dp[0][0];
+    }
+};
